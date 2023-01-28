4197a68f897da8643f75d0485665f9b61fe34583
BBJohns05 <thepurpleblur@gmail.com>
2023-01-28 10:12:45 -0700

    start pt2
    Centered items, added buttons, attempted to make johnson button move when hovered

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ var should_play=true
         }
 
     }
+
+const space = document.querySelector(".space")    
+const johnson = document.querySelector(".johnson");
+
+johnson.addEventListener("moseover", () => {
+    const i = math.floor(Math.random() * space.width - johnson.width)+1;
+    const j = Math.floor(Math.random() * space.height - johnson.height)+1;
+    johnson.style.left= i + 'px';
+    joshnson.style.top= j + 'px';
+})    
